fafac6b84bd1fea2cd1203361c41d135658ce454
Omar Almalol <omar.alma3lol@gmail.com>
2023-09-13 21:34:46 +0200

    feat(tmi): adds endpoints to login & creates tmi bot (closes #5)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  main(@Res() res: Response) {
+    return res.render('index', { client_id: process.env.CLIENT_ID });
+  }
+
+  @Get('/token')
+  async getToken(
+    @Query()
+    query:
+      | { code: string; scope: string }
+      | { error: string; error_description: string },
+    @Res() res: Response,
+  ) {
+    if ('code' in query) {
+      try {
+        const result = await this.appService.getToken(query.code);
+        this.appService.startBot(result.access_token);
+        return res.render('success', {});
+      } catch (e) {
+        console.log(e);
+        return res.render('error', {
+          error: `Server couldn't authorize! (Twitch Response status: ${e.response.data.status} - ${e.response.data.message})`,
+        });
+      }
+    } else {
+      return res.render('error', {
+        error: 'User denied access to the app.',
+      });
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,78 @@
 import { Injectable } from '@nestjs/common';
+import Axios from 'axios';
+import { Client } from 'tmi.js';
+
+type TwitchOAuth2TokenResponse = {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string[];
+  token_type: string;
+};
+
+type TwitchOAuth2ValidationResponse = {
+  expires_in: number;
+  scope: string[];
+  client_id: string;
+  login: string;
+  user_id: string;
+};
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  getToken = async (code: string) => {
+    const response = await Axios.post<TwitchOAuth2TokenResponse>(
+      'https://id.twitch.tv/oauth2/token',
+      {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:3000/token',
+      },
+      {
+        responseType: 'json',
+      },
+    );
+    return response.data;
+  };
+
+  validateToken = async (token: string) => {
+    const response = await Axios.get<TwitchOAuth2ValidationResponse>(
+      'https://id.twitch.tv/oauth2/validate',
+      {
+        responseType: 'json',
+        headers: {
+          Authorization: `OAuth ${token}`,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  startBot = async (access_token: string) => {
+    const client = new Client({
+      options: { debug: true },
+      identity: {
+        username: process.env.BOT_USERNAME,
+        password: access_token,
+      },
+      channels: [process.env.CHANNEL],
+    });
+
+    client.connect();
+
+    client.on('chat', (channel, user, message, self) => {
+      if (self) return;
+      if (user['message-type'] === 'chat') {
+        if (message.toLowerCase() === '!hello') {
+          client.say(channel, `@${user.username}, heya!`);
+        }
+      }
+    });
+    client.on('join', (channel, username, self) => {
+      if (self) return;
+      client.say(channel, `Welcome aboard @${username}`);
+    });
+  };
 }
