3cf23985172b29f07bffbc8d178cf709fd4b1cbb
Omar Almalol <omar.alma3lol@gmail.com>
2023-09-17 19:15:06 +0200

    feat(frontend): replaces redux template with typescript

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,14 +1,9 @@
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./redux";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeTruthy();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code className="bg-black rounded text-white px-2">src/App.tsx</code> and save to reload please.
-				</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
